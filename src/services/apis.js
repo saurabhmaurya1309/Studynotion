c690ca03ee00da9421bf1c6cf3cbaedf3800c543
saurabhmaurya1309 <saurabhmaurya1309@gmail.com>
2024-06-16 17:01:36 +0530

    some changes

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL
-const BASE_URL="http://localhost:4000/api/v1"
-// const BASE_URL="https://studynotion-backend-n1zf.onrender.com"
+// const BASE_URL="http://localhost:4000/api/v1"
+const BASE_URL="https://studynotion-lypt.onrender.com/api/v1"
 
 
 // AUTH ENDPOINTS
